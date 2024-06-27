f2e484d760ca843a5c0c08ebfaee12f771ed5abf
Max Michael Jakob Lesser <max.m.lesser@nasa.gov>
2024-06-27 18:49:49 +0000

    GWCFS-2634 General - Update Export Control Statements

--- fsw/tables/cf_ips.c
+++ fsw/tables/cf_ips.c
@@ -1,21 +1,13 @@
-/************************************************************************
- * NASA Docket No. GSC-18,447-1, and identified as “CFS CFDP (CF)
- * Application version 3.0.0”
- *
- * Copyright (c) 2019 United States Government as represented by the
- * Administrator of the National Aeronautics and Space Administration.
- * All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ************************************************************************/
+/************************************************************************/
+/** \export_control EAR ECCN 9E515.a and/or 9E515.f (HALO)
+*   Export Administration Regulations (EAR) Notice
+*   This document contains information which falls under the purview of the Export Administration Regulations (EAR),
+*   15 CFR §730-774 and is export controlled. It may be used only to fulfill responsibilities of the Parties of,
+*   or a Cooperating Agency of a NASA Gateway Program Partner (CSA, ESA, JAXA, MBRSC) and their contractors in
+*   furtherance of the Gateway MOUs with ESA, CSA, and Japan and IA with MBRSC. Any use, re-transfer,
+*   or disclosure to any party for any purpose other than the designated use of fulfilling the responsibilities
+*   of the Gateway MOUs and IA requires prior U.S. Government authorization.
+*************************************************************************/
 
 /**
  * @file
